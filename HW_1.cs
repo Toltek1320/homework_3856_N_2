9dcd65f34f13c8a9f527fafbeca4b5991dd39993
Ruslan <123066002+Toltek1320@users.noreply.github.com>
2023-01-31 23:43:07 +0300

    Update HW_1.cs
    Homework_seminar_2_Lapshin_R_V

--- HW_1.cs
+++ HW_1.cs
@@ -16,3 +16,47 @@ else
 {
     Console.WriteLine("Это число не трехзначное!");
 }
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+string strNumber = Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+int Length = number.ToString().Length;
+if (Length >= 3)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine("Третья цифра числа -> " + result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+Console.WriteLine("Введите день недели: ");
+string strNumber = Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+if (number >= 1 && number <= 5)
+{
+    Console.WriteLine("Это рабочий день");
+}
+else if (number >= 6 && number <= 7)
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это не день недели");
+}
